ac3c015d7f71c599dc9bd06965458cc264c0685a
Raj Dave <10864@bpsdoha.edu.qa>
2024-06-02 19:34:29 +0300

    add sample code to do stuff when navbar elements are clicked

--- gui/static/homepage.js
+++ gui/static/homepage.js
@@ -1,1 +1,23 @@
-console.log("Hello World")
+function createUnreadNotificationBox(title, description, image_path, timestamp) {
+
+}
+
+
+
+
+// Wait for window to completely load
+window.addEventListener('DOMContentLoaded', function() {
+
+    const elements = document.getElementsByClassName('nav-item')
+
+    for (let i = 0; i < elements.length; i++) {
+        elements[i].onclick = () => {
+            elements[i].classList.add('selected')
+            console.log(`${elements[i].innerText}`);
+        }
+    }
+
+});
+
+
+
