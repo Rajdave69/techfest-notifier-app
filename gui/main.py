96c82ceb1a42b17b94b70a0f16f8e0a7d4282c7d
Raj Dave <10864@bpsdoha.edu.qa>
2024-06-03 18:49:05 +0300

    add pywebview to requirements and main.py

--- gui/main.py
+++ gui/main.py
@@ -1,8 +1,21 @@
 from flask import Flask
 from flask import render_template
+import webview
 
 app = Flask(__name__, template_folder="./templates")
+
 
 @app.route("/")
 def root():
     return render_template('homepage.html')
+
+
+@app.route('/potato')
+def potato():
+    return render_template()
+
+
+# webview.create_window('Notifier App', app, width=1920, height=1080)
+
+# webview.start(debug=True)  # gui='mshtml' if it doesn't normally work
+# webview.resize()
