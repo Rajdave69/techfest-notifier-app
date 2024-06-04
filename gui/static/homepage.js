e9679f552aaa60f71c84407f6f33a4835d4dd4aa
Raj Dave <10864@bpsdoha.edu.qa>
2024-06-04 17:20:36 +0300

    add JS for functional sidebar

--- gui/static/homepage.js
+++ gui/static/homepage.js
@@ -8,16 +8,53 @@ function createUnreadNotificationBox(title, description, image_path, timestamp) 
 // Wait for window to completely load
 window.addEventListener('DOMContentLoaded', function() {
 
-    const elements = document.getElementsByClassName('nav-item')
+    // Sidebar handler
+    const elements = document.getElementById('sidebar-ul')
 
-    for (let i = 0; i < elements.length; i++) {
-        elements[i].onclick = () => {
-            elements[i].classList.add('selected')
-            console.log(`${elements[i].innerText}`);
+    for (let i = 0; i < elements.children.length; i++) {
+            elements.children[i].onclick = function() {
+                // Remove 'selected' class from all elements
+                for (let j = 0; j < elements.children.length; j++) {
+                    elements.children[j].classList.remove('selected');
+                }
+
+                // Add 'selected' class to the clicked element
+                this.classList.add('selected');
+
+            };
         }
+
+
+    // Function to show the appropriate div based on the hash
+    function showDivBasedOnHash() {
+
+        // Get the divs
+        const allDivs = {
+            '#': document.getElementById("#"),
+            '#settings': document.getElementById("#settings"),
+            '#reminders': document.getElementById("#reminders"),
+            "#notification-history": document.getElementById("#notification-history")
+        }
+
+        function hideAllDivsExcept(except) {
+            for (let divKey in allDivs) {
+                allDivs[divKey].hidden = divKey !== except;
+            }
+        }
+
+
+        if (window.location.hash in allDivs) {
+            hideAllDivsExcept(window.location.hash)
+        } else {
+            hideAllDivsExcept('#')
+        }
+
     }
 
+    // Check the hash initially when the page loads
+    showDivBasedOnHash();
+
+    // Listen for hash changes in the URL
+    window.addEventListener("hashchange", showDivBasedOnHash);
+
 });
-
-
-
