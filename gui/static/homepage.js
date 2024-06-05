ed58cb1edf6687c5065b5acf5b57d3ae2908cf04
Raj Dave <10864@bpsdoha.edu.qa>
2024-06-05 22:27:18 +0300

    major js changes and improvements

--- gui/static/homepage.js
+++ gui/static/homepage.js
@@ -1,5 +1,99 @@
 const API_URL = ""
 
+function showDivBasedOnHash() {
+    if (window.location.hash === "") {
+        window.location.hash = "#"
+    }
+
+        // Get the divs
+        const allDivs = {
+            '#': document.getElementById("#"),
+            '#settings': document.getElementById("#settings"),
+            '#reminders': document.getElementById("#reminders"),
+            "#notification-history": document.getElementById("#notification-history"),
+            '#create-reminder': document.getElementById("#create-reminder")
+        }
+
+        function hideAllDivsExcept(except) {
+            for (let divKey in allDivs) {
+
+                allDivs[divKey].hidden = divKey !== except;
+
+            }
+        }
+
+
+        if (window.location.hash in allDivs) {
+            hideAllDivsExcept(window.location.hash)
+            return window.location.hash
+        } else {
+            hideAllDivsExcept('#')
+        }
+
+    }
+
+function homepageLoadUnreadNotifications() {
+        fetch(`API_URL`, {method: 'GET'})
+        .then(r => {
+
+        /*
+        const r = [
+            {'title': 'Title', 'content': 'Content', 'image': './static/placeholder_image.png', 'epoch': '123'},
+            {'title': 'Title', 'content': 'Content', 'image': './static/placeholder_image.png', 'epoch': '123'},
+            {'title': 'Title', 'content': 'Content', 'image': './static/placeholder_image.png', 'epoch': '123'},
+            {'title': 'Title', 'content': 'Content', 'image': './static/placeholder_image.png', 'epoch': '123'},
+            {'title': 'Title', 'content': 'Content', 'image': './static/placeholder_image.png', 'epoch': '123'},
+        ]*/
+
+            const unreadNotificationBox = document.getElementById('unread-notifications-box')
+
+            for (let notification of r) {
+                console.log(notification)
+                // Create the unread-notification div
+                const mainDiv = document.createElement('div')
+
+                // Create the required items to be put in the div
+                const image = document.createElement('img')
+                const h3 = document.createElement('h3')
+                const p = document.createElement('p')
+
+                // Assign values to the divs
+                mainDiv.setAttribute('class', 'notification-box');
+                image.src = notification['image']
+                h3.innerText = notification['title']
+                p.innerText = notification['content']
+
+                // Add all the new items to the main div, then add the main div to the page
+                mainDiv.append(image, h3, p)
+                unreadNotificationBox.append(mainDiv)
+            }
+
+            if (r.length === 0) {
+                document.getElementById('unread-notifs-number-box').style.display = 'none';
+                document.getElementById('unread-notifications').checked = false
+            } else if (r.length >= 0) {
+                document.getElementById('unread-notifs-number-box').innerText = r.length.toString()
+                document.getElementById('unread-notifs-number-box').style.display = 'flex';
+                document.getElementById('unread-notifications').checked = true
+
+            } else {
+                console.error("unreadNotificationBox list from API is less than 0 elements long (possible undefined)")
+            }
+        })
+    }
+
+
+function pageLoad() {
+        // Check the hash initially when the page loads
+        const currentPage = showDivBasedOnHash();
+
+        if (currentPage === '#') {
+            homepageLoadUnreadNotifications();
+        }
+
+    }
+
+
 
 // Wait for window to completely load
 window.addEventListener('DOMContentLoaded', function() {
@@ -20,90 +114,8 @@ window.addEventListener('DOMContentLoaded', function() {
             };
         }
 
-
-    // Function to show the appropriate div based on the hash
-    function showDivBasedOnHash() {
-
-        // Get the divs
-        const allDivs = {
-            '#': document.getElementById("#"),
-            '#settings': document.getElementById("#settings"),
-            '#reminders': document.getElementById("#reminders"),
-            "#notification-history": document.getElementById("#notification-history")
-        }
-
-        function hideAllDivsExcept(except) {
-            for (let divKey in allDivs) {
-                allDivs[divKey].hidden = divKey !== except;
-            }
-        }
-
-
-        if (window.location.hash in allDivs) {
-            hideAllDivsExcept(window.location.hash)
-        } else {
-            hideAllDivsExcept('#')
-        }
-
-    }
-
-
-    function homepageLoadUnreadNotifications() {
-        fetch(`API_URL`, {method: 'GET'})
-        .then(r => {
-
-        /*
-        const r = [
-            {'title': 'Title', 'content': 'Content', 'image': './static/placeholder_image.png', 'epoch': '123'},
-            {'title': 'Title', 'content': 'Content', 'image': './static/placeholder_image.png', 'epoch': '123'},
-            {'title': 'Title', 'content': 'Content', 'image': './static/placeholder_image.png', 'epoch': '123'},
-            {'title': 'Title', 'content': 'Content', 'image': './static/placeholder_image.png', 'epoch': '123'},
-            {'title': 'Title', 'content': 'Content', 'image': './static/placeholder_image.png', 'epoch': '123'},
-        ]*/
-
-            const unreadNotificationBox = document.getElementById('unread-notifications-box')
-            for (let notification of r) {
-                console.log(notification)
-                const mainDiv = document.createElement('div')
-                mainDiv.setAttribute('class', 'unread-notification');
-
-                const image = document.createElement('img')
-                image.src = notification['image']
-                const h3 = document.createElement('h3')
-                h3.innerText = notification['title']
-                const p = document.createElement('p')
-                p.innerText = notification['content']
-
-                mainDiv.append(image, h3, p)
-                unreadNotificationBox.append(mainDiv)
-            }
-
-            if (r.length === 0) {
-                document.getElementById('unread-notifs-number-box').style.display = 'none';
-                document.getElementById('unread-notifications').checked = false
-            } else if (r.length >= 0) {
-                document.getElementById('unread-notifs-number-box').innerText = r.length.toString()
-                document.getElementById('unread-notifs-number-box').style.display = 'flex';
-                document.getElementById('unread-notifications').checked = true
-
-            } else {
-                console.error("unreadNotificationBox list from API is less than 0 elements long (possible undefined)")
-            }
-        })
-    }
-
-    showDivBasedOnHash();
+    // Run it once on page load
     pageLoad();
-
-    function pageLoad() {
-        // Check the hash initially when the page loads
-        showDivBasedOnHash();
-
-        //
-        homepageLoadUnreadNotifications();
-
-    }
-
 
     // Listen for hash changes in the URL
     window.addEventListener("hashchange", pageLoad);
