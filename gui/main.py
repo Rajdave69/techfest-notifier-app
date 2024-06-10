0739fee84b41c699bc74b082780bcf7e15e51267
Raj Dave <10864@bpsdoha.edu.qa>
2024-06-10 22:40:37 +0300

    fix many main.py issues

--- gui/main.py
+++ gui/main.py
@@ -3,7 +3,6 @@ from flask import render_template, request
 import webview
 import sqlite3
 
-
 app = Flask(__name__, template_folder="./templates")
 
 
@@ -11,10 +10,12 @@ def setup_db():
     db = sqlite3.connect("database.db")
     c = db.cursor()
 
-    c.execute("CREATE TABLE IF NOT EXISTS notifications (id INTEGER PRIMARY KEY AUTOINCREMENT, type TEXT, read INT, title TEXT, body TEXT, sender TEXT, image_url TEXT, timestamp INT)")
+    c.execute(
+        "CREATE TABLE IF NOT EXISTS notifications (id INTEGER PRIMARY KEY AUTOINCREMENT, type TEXT, read INT, title TEXT, body TEXT, sender TEXT, image_url TEXT, timestamp INT)")
     db.commit()
 
     db.close()
+
 
 setup_db()
 
@@ -26,60 +27,60 @@ def root():
 
 @app.route('/api/reminders/')
 def reminders():
-    
     db = sqlite3.connect("database.db")
     c = db.cursor()
 
-    c.execute('SELECT id, title, body, timestamp FROM notifications WHERE type="reminders"')
-    reminders = sorted(c.fetchall(), key=lambda x:x[-1], reverse=True)
+    c.execute('SELECT id, title, body, timestamp FROM notifications WHERE type="reminder"')
+    reminders_ = sorted(c.fetchall(), key=lambda x: x[-1], reverse=True)
 
     db.close()
 
     return {
         'http_code': 200,
-        'data': [{'title':r[1], 'description':r[2], 'timestamp':r[-1], 'id':r[0]} for r in reminders]
-
+        'data': [{'title': r[1], 'description': r[2], 'timestamp': r[-1], 'id': r[0]} for r in reminders_]
     }
+
 
 @app.route('/api/notifications/unread/')
 def unread_notifications():
-
     db = sqlite3.connect("database.db")
     c = db.cursor()
 
     req = ['id', 'type', 'title', 'body', 'sender', 'image_url', 'timestamp']
 
     c.execute(f'SELECT {', '.join(req)} FROM notifications WHERE read=0')
-    unread = sorted(c.fetchall(), key=lambda x:x[-1], reverse=True)
-    
+    unread = sorted(c.fetchall(), key=lambda x: x[-1], reverse=True)
+
     db.close()
 
     return {
         "http_code": 200,
-        'data': [{req[i]:u[i] for i in range(len(req))} for u in unread]
+        'data': [{req[i]: u[i] for i in range(len(req))} for u in unread]
     }
+
 
 @app.route("/api/notifications/")
 def notifications():
-
     db = sqlite3.connect("database.db")
     c = db.cursor()
 
     req = ['id', 'type', 'read', 'title', 'body', 'sender', 'image_url', 'timestamp']
 
     c.execute(f'SELECT {', '.join(req)} FROM notifications')
-    notif = sorted(c.fetchall(), key=lambda x:x[-1], reverse=True)
-    
+    notif = sorted(c.fetchall(), key=lambda x: x[-1], reverse=True)
+
     db.close()
 
     return {
         "http_code": 200,
-        'data': [{req[i]:u[i] for i in range(len(req))} for u in notif]
+        'data': [{req[i]: u[i] for i in range(len(req))} for u in notif]
     }
+
 
 @app.route("/api/reminders/delete/", methods=['POST'])
 def delete_reminder():
-    _id = request.form['id']
+    print(request.json)
+    _id = request.json['id']
     db = sqlite3.connect("database.db")
     c = db.cursor()
 
@@ -90,29 +91,24 @@ def delete_reminder():
 
     return {"http_code": 200}
 
+
 @app.route("/api/reminders/create/", methods=['POST'])
 def create_reminder():
-    print(request)
-    print(request.json)
-    print(request.form.get('description'))
-    print("hello")
     name = request.json["name"]
     timestamp = request.json["timestamp"]
-    image_url = request.json["image_url"] 
+    image_url = request.json["image_url"]
     desc = request.json['description']
-    print(name, timestamp, image_url, desc)
 
     db = sqlite3.connect("database.db")
     c = db.cursor()
 
-    c.execute("INSERT INTO notifications values(NULL,?,?,?,?,?,?,?)", (
+    c.execute("INSERT INTO notifications values (NULL,?,?,?,?,?,?,?)", (
         'reminder', 0, name, desc, 0, image_url, timestamp))
     db.commit()
 
     db.close()
 
-    return {'http_code':200}
-
+    return {'http_code': 200}
 
 
 webview.create_window('Notifier App', app, width=1920, height=1080)
