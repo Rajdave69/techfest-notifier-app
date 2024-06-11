91e45dce1f25917ac2bc06255e22fb7d50ae77d1
Raj Dave <10864@bpsdoha.edu.qa>
2024-06-11 23:45:30 +0300

    a few more changes

--- gmail/gmail.py
+++ gmail/gmail.py
@@ -18,8 +18,8 @@ from mimetypes import guess_type as guess_mime_type
 import email
 import base64
 
-
 SCOPES = ['https://www.googleapis.com/auth/gmail.modify']
+
 
 class GMAIL:
 
@@ -43,14 +43,14 @@ class GMAIL:
                 pickle.dump(creds, token)
         return build('gmail', 'v1', credentials=creds)
 
-    def get_message(self, msg_id):
+    def get_email(self, msg_id):
 
         try:
             message_list = self.service.users().messages().get(userId='me', id=msg_id, format='raw').execute()
 
             msg_raw = base64.urlsafe_b64decode(message_list['raw'].encode('ASCII'))
             msg_str = email.message_from_bytes(msg_raw)
-            
+
             content_type = msg_str.get_content_maintype()
 
             if content_type == 'multipart':
@@ -61,34 +61,33 @@ class GMAIL:
             else:
                 body = msg_str.get_payload(decode=True).decode('utf-8')
 
-            return {'subject':msg_str['subject'], 'sender':msg_str['from'], 'body':body, 'timestamp':msg_str['Date'], 'id':msg_id}
-
+            return {'subject': msg_str['subject'], 'sender': msg_str['from'], 'body': body,
+                    'timestamp': msg_str['Date'], 'id': msg_id}
 
         except HttpError as error:
             print(f'An error occurred: {error}')
 
-    def search_messages(self):
+    def search_emails(self):
         result = self.service.users().messages().list(userId='me', labelIds="UNREAD").execute()
         number_result = result['resultSizeEstimate']
 
         final_list = []
 
-        if number_result>0:
+        if number_result > 0:
             message_ids = result['messages']
 
             for ids in message_ids:
                 final_list.append(ids['id'])
-                self.get_message(self.service, ids['id'] )
-                
+                self.get_email(self.service, ids['id'])
+
             return final_list
 
     def mark_read(self, msg_id):
-        
+
         try:
-            self.service.users().messages().modify(userId='me', id=msg_id, body={'removeLabelIds':'UNREAD'}).execute()
+            self.service.users().messages().modify(userId='me', id=msg_id, body={'removeLabelIds': 'UNREAD'}).execute()
             print("success")
         except HttpError as error:
             print(error, "could not mark as READ")
 
-
-#print(search_messages(service))
+# print(search_messages(service))
