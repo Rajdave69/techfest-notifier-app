848b59b978952544907dddf2517d5dc3458dee7b
TJP <69581225+JavaProgswing@users.noreply.github.com>
2024-06-13 08:44:11 +0530

    Notification fixes!

---
+++ gmail/gmail.py
@@ -0,0 +1,94 @@
+import os
+import pickle
+# Gmail API utils
+from googleapiclient.discovery import build
+from google_auth_oauthlib.flow import InstalledAppFlow
+from google.auth.transport.requests import Request
+from googleapiclient.errors import HttpError
+# for encoding/decoding messages in base64
+# from base64 import urlsafe_b64decode, urlsafe_b64encode
+# for dealing with attachement MIME types
+# from email.mime.text import MIMEText
+# from email.mime.multipart import MIMEMultipart
+# from email.mime.image import MIMEImage
+# from email.mime.audio import MIMEAudio
+# from email.mime.base import MIMEBase
+# from mimetypes import guess_type as guess_mime_type
+from datetime import datetime
+
+import email
+import base64
+
+SCOPES = ['https://www.googleapis.com/auth/gmail.modify']
+
+
+class GMAIL:
+
+    def __init__(self):
+        self.service = self.gmail_authenticate()
+
+    def gmail_authenticate(self):
+        creds = None
+        if os.path.exists("token.pickle"):
+            with open("token.pickle", "rb") as token:
+                creds = pickle.load(token)
+
+        if not creds or not creds.valid:
+            if creds and creds.expired and creds.refresh_token:
+                creds.refresh(Request())
+            else:
+                flow = InstalledAppFlow.from_client_secrets_file('./gmail/credentials.json', SCOPES)
+                creds = flow.run_local_server(port=0)
+            # save the credentials for the next run
+            with open("token.pickle", "wb") as token:
+                pickle.dump(creds, token)
+        return build('gmail', 'v1', credentials=creds)
+
+    def get_email(self, msg_id):
+
+        try:
+            message_list = self.service.users().messages().get(userId='me', id=msg_id, format='raw').execute()
+
+            msg_raw = base64.urlsafe_b64decode(message_list['raw'].encode('ASCII'))
+            msg_str = email.message_from_bytes(msg_raw)
+
+            content_type = msg_str.get_content_maintype()
+
+            if content_type == 'multipart':
+                for part in msg_str.get_payload():
+                    if part.get_content_maintype() == 'text':
+                        body = part.get_payload(decode=True).decode('utf-8')
+                        break
+            else:
+                body = msg_str.get_payload(decode=True).decode('utf-8')
+
+            return {'subject': msg_str['subject'], 'sender': msg_str['from'], 'body': body, 'id': msg_id, 
+                    'timestamp': int(datetime.strptime(msg_str['Date'], "%a, %d %b %Y %H:%M:%S %z").timestamp()) }
+
+        except HttpError as error:
+            print(f'An error occurred: {error}')
+
+    def unread_messages(self):
+        result = self.service.users().messages().list(userId='me', labelIds="UNREAD").execute()
+        number_result = result['resultSizeEstimate']
+
+        all_ids = []
+        msgs = []
+
+        if number_result > 0:
+            message_ids = result['messages']
+
+            for ids in message_ids:
+                all_ids.append(ids['id'])
+                msgs.append(self.get_email(ids['id']))
+                
+            return all_ids, msgs
+
+    def mark_read(self, msg_id):
+
+        try:
+            self.service.users().messages().modify(userId='me', id=msg_id, body={'removeLabelIds': 'UNREAD'}).execute()
+            print("success")
+        except HttpError as error:
+            print(error, "could not mark as READ")
+
---
+++ main.py
@@ -0,0 +1,385 @@
+import random
+import sqlite3
+import threading
+import time
+import webview
+from flask import Flask
+from flask import render_template, request
+from win11toast import toast
+
+app = Flask(__name__, template_folder="./gui/templates", static_folder="./gui/static")
+window = webview.create_window('Notifier App', app, width=1920, height=1080)
+
+BASE_URL = 'http://127.0.0.1:35505'
+LI = ['id', 'type', 'read', 'title', 'body', 'sender', 'image_url', 'timestamp']
+
+from gmail import gmail
+import json
+
+import threading
+
+MAIL = gmail.GMAIL()
+lock = threading.Lock()
+
+db = sqlite3.connect("database.db", check_same_thread=False, timeout=20, autocommit=True)
+c = db.cursor()
+
+
+def notification_toast_handler(id_, notif_type, notif_output: dict):
+    if notif_type == "email":
+        if notif_output['arguments'] == "http:Mark as Read":
+            mark_email_as_read(id_)
+        elif notif_output['arguments'] == "http:":
+            mark_email_as_read(id_)
+            window.evaluate_js(f"window.location.hash = 'view-email?id={id_}'")
+
+    elif notif_type == "reminder":
+        if notif_output['arguments'] == "http:Mark as Read":
+            mark_reminder_as_read(id_)
+        elif notif_output['arguments'] == "http:":
+            mark_reminder_as_read(id_)
+            window.evaluate_js(f"window.location.hash = 'view-reminder?id={id_}'")
+
+    else:
+        mark_notification_as_read(id_)
+
+
+def mark_notification_as_read(notification_id):
+    
+    try:
+        lock.acquire(True)
+        c.execute("UPDATE notifications SET read=1 WHERE id=?", (notification_id,))
+        db.commit()
+    finally:
+        lock.release()
+
+
+def mark_reminder_as_read(notification_id):
+    mark_notification_as_read(notification_id)
+
+
+def mark_email_as_read(email_id):
+    MAIL.mark_read(email_id)
+
+    try:
+        lock.acquire(True)
+        c.execute(f'UPDATE notifications SET read=1 WHERE id="{email_id}"')
+        db.commit()
+    finally:
+        lock.release()
+
+
+def send_reminder_notification(title, body, id_, image_url):
+    buttons = [
+        'Mark as Read',
+    ]
+
+    icon = {
+        'src': image_url if image_url else "",
+        'placement': 'appLogoOverride'
+    }
+
+    toast(
+        f'Reminder: {title}', body, icon=icon, buttons=buttons,
+        on_click=lambda notif_output: notification_toast_handler(id_, "reminder", notif_output),
+        on_dismissed=lambda X: X
+    )
+
+
+def send_notification(title, body, id_, image_url):
+    buttons = [
+        'Mark as Read'
+    ]
+
+    icon = {
+        'src': image_url if image_url else "",
+        'placement': 'appLogoOverride'
+    }
+
+    toast(
+        title, body, buttons=buttons, icon=icon,
+        on_click=lambda notif_output: notification_toast_handler(id_, '', notif_output),
+        on_dismissed=lambda X: X
+    )
+
+
+
+
+def send_email_notification(id_, sender, title, body, timestamp):
+    buttons = [
+        'Mark as Read'
+    ]
+
+    toast(
+        f'Email: {title}', body, buttons=buttons,
+        on_click=lambda notif_output: notification_toast_handler(id_, "email", notif_output),
+        on_dismissed=lambda X: X
+    )
+
+
+def setup_db():
+    c.execute(
+        "CREATE TABLE IF NOT EXISTS notifications (id TEXT, type TEXT, read INT, title TEXT, body TEXT, sender TEXT, image_url TEXT, timestamp INT)")
+    db.commit()
+
+
+setup_db()
+
+
+@app.route("/")
+def root():
+    return render_template('homepage.html')
+
+
+@app.route('/api/send-notification/', methods=['POST'])
+def send_notification_():
+    title = request.json['title']
+    body = request.json['body']
+    image_url = "https://img.icons8.com/?size=100&id=110472&format=png"
+    id_ = str(random.random())
+
+    try:
+        lock.acquire(True)
+        c.execute('INSERT INTO notifications values(?,?,?,?,?,?,?,?)',
+                  (id_, 'api', 0, title, body, 0, image_url, int(time.time())))
+        db.commit()
+    finally:
+        lock.release()
+
+    send_notification(title, body, id_, image_url)
+
+    return {}
+
+
+@app.route('/api/reminders/')
+def reminders():
+
+    try:
+        lock.acquire(True)
+        c.execute('SELECT id, title, body, timestamp FROM notifications WHERE type="reminder"')
+        reminders_ = c.fetchall()
+        reminders_ = sorted(reminders_, key=lambda x: x[-1], reverse=False)
+    finally:
+        lock.release()
+
+    return {
+        'http_code': 200,
+        'data': [{'title': r[1], 'body': r[2], 'timestamp': r[-1], 'id': r[0]} for r in reminders_]
+    }
+
+
+@app.route('/api/reminders/<id_>')
+def reminders_by_id(id_):
+
+    try:
+        lock.acquire(True)
+        c.execute(f'SELECT id, title, body, timestamp FROM notifications WHERE type="reminder" AND id="{id_}"')
+        reminders_ = c.fetchone()
+    finally:
+        lock.release()
+
+    return {
+        'http_code': 200,
+        'data':
+            {'title': reminders_[1], 'body': reminders_[2], 'timestamp': reminders_[-1], 'id': reminders_[0]}
+    }
+
+
+@app.route('/api/reminders/unread/')
+def unread_reminders():
+    req = ['id', 'type', 'title', 'body', 'sender', 'image_url', 'timestamp']
+
+    try:
+        lock.acquire(True)
+        c.execute(f"SELECT {', '.join(req)} FROM notifications WHERE read=0 AND timestamp <= {int(time.time())} AND type='reminder'")
+        unread = sorted(c.fetchall(), key=lambda x: x[-1], reverse=False)
+    finally:
+        lock.release()
+
+    return {
+        "http_code": 200,
+        'data': [{req[i]: u[i] for i in range(len(req))} for u in unread]
+    }
+
+
+@app.route('/api/notifications/unread/')
+def unread_notifications():
+    req = ['id', 'type', 'title', 'body', 'sender', 'image_url', 'timestamp']
+
+    try:
+        lock.acquire(True)
+        c.execute(f'SELECT {", ".join(req)} FROM notifications WHERE read=0 AND timestamp <= {int(time.time())}')
+        unread = sorted(c.fetchall(), key=lambda x: x[-1], reverse=True)
+    finally:
+        lock.release()
+
+    return {
+        "http_code": 200,
+        'data': [{req[i]: u[i] for i in range(len(req))} for u in unread]
+    }
+
+
+@app.route('/api/emails/')
+def get_emails():
+
+    try:
+        lock.acquire(True)
+        c.execute('SELECT * FROM notifications WHERE type="email"')
+        data = c.fetchall()
+    finally:
+        lock.release()
+
+    return {
+        "http_code": 200,
+        "data": [{LI[i]: d[i] for i in range(len(d))} for d in data]
+    }
+
+
+@app.route('/api/emails/<id_>')
+def get_email_by_id(id_):
+    print(id_)
+    try:
+        lock.acquire(True)
+        c.execute(f'SELECT * FROM notifications WHERE type="email" AND id="{id_}"')
+        data = c.fetchone()
+    finally:
+        lock.release()
+
+    return {
+        "http_code": 200,
+        "data": {'title': data[3], 'body': data[4], 'timestamp': data[-1], 'id': data[0], 'sender': data[5]}
+    }
+
+
+@app.route('/api/emails/unread/')
+def get_unread_emails():
+    try:
+        lock.acquire(True)
+        c.execute('SELECT * FROM notifications WHERE type="email" AND read=0')
+        data = c.fetchall()
+    finally:
+        lock.release()
+
+    return {
+        "http_code": 200,
+        "data": [{LI[i]: d[i] for i in range(len(d))} for d in data]
+    }
+
+
+@app.route("/api/notifications/")
+def notifications():
+    req = ['id', 'type', 'read', 'title', 'body', 'sender', 'image_url', 'timestamp']
+
+    try:
+        lock.acquire(True)
+        c.execute(f'SELECT {", ".join(req)} FROM notifications')
+        notif = sorted(c.fetchall(), key=lambda x: x[-1], reverse=True)
+    finally:
+        lock.release()
+
+    return {
+        "http_code": 200,
+        'data': [{req[i]: u[i] for i in range(len(req))} for u in notif]
+    }
+
+
+@app.route("/api/reminders/delete/<id_>", methods=['DELETE'])
+def delete_reminder(id_):
+
+    try:
+        lock.acquire(True)
+        c.execute(f"DELETE FROM notifications WHERE id={id_}")
+        db.commit()
+    finally:
+        lock.release()
+
+    return {"http_code": 200}
+
+
+@app.route("/api/reminders/create/", methods=['POST'])
+def create_reminder():
+    name = request.json["name"]
+    timestamp = request.json["timestamp"]
+    image_url = request.json["image_url"]
+    desc = request.json['body']
+
+    if not image_url:
+        image_url = "https://img.icons8.com/?size=100&id=113847&format=png"
+
+    with open('reminder.json', 'r') as f:
+        count = json.load(f)['count']
+
+    with open('reminder.json', 'w') as f:
+        json.dump({'count': count + 1}, f)
+
+    try:
+        lock.acquire(True)
+        c.execute("INSERT INTO notifications values (?,?,?,?,?,?,?,?)",
+                (str(count + 1), 'reminder', 0, name, desc, 0, image_url, timestamp))
+        db.commit()
+    finally:
+        lock.release()
+
+    return {'http_code': 200}
+
+
+def add_emails(emails):
+    try:
+        lock.acquire(True)
+        c.execute('SELECT id FROM notifications WHERE type="email"')
+        old = [i[0] for i in c.fetchall()]
+    finally:
+        lock.release()
+
+    emails = [email for email in emails if email['id'] not in old]
+
+    try:
+        lock.acquire(True)
+        for mail in emails:
+            c.execute("INSERT INTO notifications values (?,?,?,?,?,?,?,?)", 
+                    (mail['id'], 'email', 0, mail['subject'], mail['body'], mail['sender'],
+                    "https://img.icons8.com/?size=100&id=110231&format=png", mail['timestamp']))
+            
+            send_email_notification(title=mail['subject'], sender=mail['sender'], 
+                                    body=mail['body'], id_=mail['id'], timestamp=mail['timestamp'])
+
+        db.commit()
+    finally:
+        lock.release()
+
+def check_for_notifications():
+    def check():
+        while True:
+
+            emails = MAIL.unread_messages()
+            print(emails, '\n' * 5)
+
+            if not emails:
+                pass
+            else:
+                add_emails(emails[1])
+
+            reminders = unread_notifications()['data']
+
+            for r in reminders:
+                if r['type'] != 'email':
+                    send_reminder_notification(title=r['title'], body=r['body'], id_=r['id'],
+                                               image_url=r['image_url'])
+
+            time.sleep(15)
+
+    thread = threading.Thread(target=check)
+    thread.daemon = True
+    thread.start()
+
+
+check_for_notifications()
+
+webview.start(debug=True)
+
+"""
+rajs todo list
+
+lightmode darkmode conversion
+settings page
+"""
